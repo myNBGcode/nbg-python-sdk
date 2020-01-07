6d8e18d933119c397ca9861fac801474f3dbdc6f
Paris Kasidiaris <paris@sourcelair.com>
2020-01-07 23:34:00 +0000

    Implement foundation for all consent methods.

--- nbg/base/auth.py
+++ nbg/base/auth.py
@@ -52,3 +52,15 @@ class AuthenticatedClientMixin:
 
     def set_access_token(self, access_token: str):
         self._access_token = access_token
+
+    def generate_consent(self) -> dict:
+        data = {"applicationId": self.client_id}
+        return self._api_request("POST", "consents/request-access", data)
+
+    def get_consent_information(self, user_id: str, consent_id: str) -< dict:
+        data = {"applicationId": self.client_id, "userId": user_id, "consentId": consent_id}
+        return self._api_request("POST", "consents/info", data)
+
+    def delete_consent(self, user_id: str, consent_id: str, tan_number: str) -< dict:
+        data = {"userId": user_id, "consentId": consent_id, "tanNumber": tan_number}
+        return self._api_request("POST", "consents/delete", data)
